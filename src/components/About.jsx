23ccf2c236999f13ab2a4b033f9aff3caf971004
Roshan kr singh <roshan.kr.singh9911@gmail.com>
2025-03-06 06:43:02 +0530

    third

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,26 +8,25 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    tiltMaxAngleX={45}
+    tiltMaxAngleY={45}
+    scale={1}
+    transitionSpeed={450}
+    className="xs:w-[250px] w-full"
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -53,7 +52,10 @@ const About = () => {
         intelligent, scalable solutions that push the boundaries of AI. From
         fine-tuning LLMs to deploying real-world AI applications, I love solving
         complex problems with cutting-edge technology.
-        <span> 𝐹𝑒𝑒𝑙 𝑓𝑟𝑒𝑒 𝑡𝑜 𝑟𝑒𝑎𝑐ℎ 𝑜𝑢𝑡—𝐼’𝑚 𝑎𝑙𝑤𝑎𝑦𝑠 𝑢𝑝 𝑓𝑜𝑟 𝑎 𝑔𝑜𝑜𝑑 𝑐𝑜𝑛𝑣𝑒𝑟𝑠𝑎𝑡𝑖𝑜𝑛!</span>
+        <span>
+          {" "}
+          𝐹𝑒𝑒𝑙 𝑓𝑟𝑒𝑒 𝑡𝑜 𝑟𝑒𝑎𝑐ℎ 𝑜𝑢𝑡—𝐼’𝑚 𝑎𝑙𝑤𝑎𝑦𝑠 𝑢𝑝 𝑓𝑜𝑟 𝑎 𝑔𝑜𝑜𝑑 𝑐𝑜𝑛𝑣𝑒𝑟𝑠𝑎𝑡𝑖𝑜𝑛!
+        </span>
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
